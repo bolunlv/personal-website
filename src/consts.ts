883794c6966d0ae3e6246d9f584589fb13fec4cb
lvbolun <bolun.lu@eoitek.com>
2023-02-24 22:28:47 +0800

    修改站点标题

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // 站点标题
-export const SITE_TITLE = "LBL的个人站点";
+export const SITE_TITLE = "吕博伦的个人站点";
 
 // 站点描述
 export const SITE_DESCRIPTION = 'Welcome to my website!';
